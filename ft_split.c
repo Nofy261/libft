834f98bc9517045ec47d18979866c1f0ed3da16c
Nofy Leclercq <nolecler@2G5.42angouleme.fr>
2024-10-12 12:32:05 +0200

    change ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 13:06:57 by nolecler          #+#    #+#             */
-/*   Updated: 2024/10/11 15:15:48 by nolecler         ###   ########.fr       */
+/*   Updated: 2024/10/12 12:28:39 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static size_t count_words(const char *s, char c)
+static size_t count_words(const char *s, char c)//static = la fonction ne peut etre appelee que dans ce fichier
 {
     size_t  i;
     size_t  counter;
@@ -29,7 +29,7 @@ static size_t count_words(const char *s, char c)
                 counter++; //je compte le mot 
                 is_word = 1;//je ne suis plus dans un mot
             }
-	}
+		}
         else
         {
             is_word = 0;//je ne suis pas dans un mot
@@ -40,20 +40,20 @@ static size_t count_words(const char *s, char c)
 }
 
 
-static void	fill_tab(char *new, char const *s, char c) // On prend un mot et on le met dans un tableau
+static void	fill_result(char *new, char const *str, char c) // On prend un mot et on le met dans un tableau
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i] && s[i] != c)
+	while (str[i] && str[i] != c)
 	{
-		new[i] = s[i];
+		new[i] = str[i];
 		i++;
 	}
 	new[i] = '\0';
 }
 
-static void	set_mem(char **tab, char const *s, char c)
+static void	set_mem(char **result, char const *str, char sep)//remplit result et alloue de la memoire pour chaque mot
 {
 	size_t	count;
 	size_t	j;
@@ -61,37 +61,37 @@ static void	set_mem(char **tab, char const *s, char c)
 
 	j = 0;
 	i = 0;
-	while (s[j])
+	while (str[j])
 	{
-		count = 0;
-		while (s[j + count] && s[j + count] != c)
-			count++;
-		if (count > 0)
+		count = 0;//on initialise count a 0 car on compte un nouveau mot 
+		while (str[j + count] && str[j + count] != sep)// D abord on compte le nombre de lettre dans un mot
+			count++;// count prend 1 
+		if (count > 0)  
 		{
-			tab[i] = malloc(sizeof(char) * (count + 1));
-			if (!tab[i])
+			result[i] = malloc(sizeof(char) * (count + 1));//puis on alloue de la memoire pour un mot + '\0'
+			if (!result[i])
 				return ;
-			fill_tab(tab[i], (s + j), c);
-			i++;
-			j = j + count;
+			fill_result(result[i], (str + j), sep);//on remplit result avec les lettres de str a l index de j
+			i++;// on passe a l index suivant de result cad un nouveau mot
+			j = j + count;//on avance a la position suivante dans la chaine str
 		}
 		else
-			j++;
+			j++;//on passe a un nouveau mot 
 	}
-	tab[i] = 0;// '\0';
+	result[i] = 0;// fin de tableau de chaine de caractere et non '\0'
 }
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	words;
-	char	**tab;
+	char	**result;//ptr vers un tab de ptr de caract
 
-	words = count_words(s, c);
-	tab = malloc(sizeof(char *) * (words + 1));
-	if (!tab)
+	words = count_words(s, c);//nombre de mots dans s 
+	result = malloc(sizeof(char *) * (words + 1));//j'alloue de la memoire pour char ** cad le tableau de tableau
+	if (!result)
 		return (NULL);
-	set_mem(tab, s, c);
-	return (tab);
+	set_mem(result, s, c);
+	return (result);
 }
 #include <stdio.h>
 
